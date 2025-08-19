18ad55f703961d4764fbda7ce0ad016ff4184bd1
sofiiir <sofiiirodas@gmail.com>
2025-08-19 11:46:39 -0700

    joins

---
+++ wrangling-practice.R
@@ -0,0 +1,75 @@
+# clearing environment
+rm(list=ls())
+
+# attach packages
+library(tidyverse)
+library(palmerpenguins)
+library(lubridate) # help us work with dates
+
+# data wrangling refresher
+# 1. only include penguins at Briscoe and Dream Islands
+# 2 remove the year and sex variables
+# 3 add a new column called body_mass_kg with penguin mass converted from grams to kg
+# 4 rename island variable to location
+
+penguins |>
+  filter(island %in% c("Briscoe", "Dream")) |>
+  select(!c(year, sex)) |>
+  mutate("body_mass_kg" = body_mass_g / 1000) |>
+  rename(location = island)
+
+# limit to Adelie penguins
+# remove any observations where flipper_length_mm is NA
+# group the data by sex
+#find the mean, sd, and sample size n() of flipper lengths for male and females
+penguins |>
+  filter(species == "Adelie") |>
+  filter(!is.na(flipper_length_mm),
+         !is.na(sex)) |> # remove rows that are not na
+  group_by(sex) |>
+  summarise(mean = mean(flipper_length_mm),
+            standard_dev = sd(flipper_length_mm),
+            sample_size = n())
+
+# practice with joins
+animals <- data.frame(
+  stringsAsFactors = FALSE,
+  location = c("lagoon", "bluff", "creek", "oaks", "bluff"),
+  species = c("bobcat", "coyote", "fox", "squirrel", "bobcat"),
+  maturity = c("adult", "juvenile", "adult", "juvenile", "adult")
+)
+
+sites <- data.frame(
+  stringsAsFactors = FALSE,
+          location = c("beach", "lagoon", "bluff", "oaks"),
+    full_site_name = c("Goleta Beach","UCSB Lagoon",
+                       "Ellwood Mesa","Fremont Campground"),
+      jurisdiction = c("SB City", "UCSB", "SB City", "USFS")
+)
+
+# tables can only be joined one at a time
+# practice with full_join
+# keeps all rows and adds all columns
+full_join(animals, sites)
+
+#left_join()
+left_join(animals, sites)
+
+#right_join()
+right_join(animals, sites)
+
+# inner_join()
+inner_join(animals, sites)
+
+#filtering joins
+semi_join(animals, sites)
+
+animals |>
+  filter(location %in% sites$location)
+
+anti_join(animals, sites)
+
+animals |>
+  filter(!location %in% sites$location)
+
+anti_join(sites, animals)
